fb908fa600eed1a6350ffd45dd3b2e859d7bef31
usadamasa <usadamasa@gmail.com>
2022-02-27 18:48:03 +0900

    fix

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/matryer/moq v0.2.3 // indirect
 	github.com/mitchellh/mapstructure v1.2.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -4,8 +4,20 @@ package graph
 // will be copied through when generating and any unknown code will be moved to the end.
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/usadamasa/hackernews/graph/generated"
+	"github.com/usadamasa/hackernews/graph/model"
 )
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
