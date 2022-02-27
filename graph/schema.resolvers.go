dad4463e21aba2d6040f5699fadaa6f799915e65
usadamasa <usadamasa@gmail.com>
2022-02-27 19:05:09 +0900

    impl todo

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,31 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/usadamasa/hackernews/graph/generated"
 	"github.com/usadamasa/hackernews/graph/model"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		Text:   input.Text,
+		UserID: input.UserID,
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
+}
+
+func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
+	return &model.User{
+		ID:   obj.UserID,
+		Name: "user " + obj.UserID,
+	}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -25,5 +39,9 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Todo returns generated.TodoResolver implementation.
+func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
